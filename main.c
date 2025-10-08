55dbe4144473c15f2daad776b2dd71d7dcb1bb01
igwgw <hanseul0204@sookmyung.ac.kr>
2025-10-08 17:46:18 +0900

    04_1

--- main.c
+++ main.c
@@ -1,44 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sumTwo(int a,int b){
-    int result;
-    result=a+b;
-    return result;
+void square (int a){
+     a=a*a;
 }
 
-int square (int n){
-    int result;
-    result=n*n;
-    return result;
-}
-
-int get_max (int x,int y){
-    int max;
-    if (x>y)
-    max=x;
-    else  
-    max=y;
-    
-    return max;
-}
-    
-
-int main(void){
-    int sum;
-    sum=sumTwo(2,3);
-    printf("sumTwo result is %i.\n",sum);
-    
-    int square_result;
-    square_result=square(3);
-    printf("square result is %i.\n",square_result);
-    
-    int max_min;
-    max_min=get_max(3,5);
-    printf("get_max result is %i.\n",max_min);
-    
-    
-
+int main(){
+    int a=2;
+    square(a);
+    printf("a=%i\n",a);
   
   system("PAUSE");	
   return 0;
