17f3aa76c9d2ea7ede1daaf8b2e39bac47d51303
igwgw <hanseul0204@sookmyung.ac.kr>
2025-10-08 16:54:11 +0900

    01

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+void print_star(int N)
+{
+     int i;
+     for(i=0;i<N;i++)
+     printf("*");
+}
+
+int main(void)
+{
+    print_star(10);
+    print_star(10);
+    print_star(10);
+
+  
+  system("PAUSE");	
+  return 0;
+}
